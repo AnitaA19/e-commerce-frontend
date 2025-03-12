44b7dc6974685aa648415c91c9f19c30a1a75f63
ANITA\anita <balasanyananita@gmail.com>
2025-03-12 19:17:26 +0400

    fix: fixed not found

--- src/services/apolloClient.js
+++ src/services/apolloClient.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'http://backend-ecommerce.atwebpages.com/', 
+    uri: '/api/', 
     headers: {
       'Content-Type': 'application/json',
     },
